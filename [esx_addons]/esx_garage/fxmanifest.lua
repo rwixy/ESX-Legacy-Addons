2285abe3b129bba22eea9736b3a4b622f0937145
_Not_ <notequalnullde@gmail.com>
2026-08-15 14:44:03 -0500

    change: update legacy version to 1.14.1 in fxmanifest.lua

--- [esx_addons]/esx_garage/fxmanifest.lua
+++ [esx_addons]/esx_garage/fxmanifest.lua
@@ -6,7 +6,7 @@ description 'Modern Garage System with React UI'
 use_experimental_fxv2_oal 'true'
 
 version '2.0.0'
-legacyversion '1.13.4'
+legacyversion '1.14.1'
 
 lua54 'yes'
 ui_page 'nui/index.html'
